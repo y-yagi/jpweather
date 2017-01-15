8fd501ed5d95bd5f02a456c6ce2fc0d88b9179e2
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2017-01-16 08:48:17 +0900

    more refactor

--- jpweather.go
+++ jpweather.go
@@ -39,6 +39,7 @@ type config struct {
 }
 
 type weatherData struct {
+	date               time.Time
 	times              []string
 	weathers           []string
 	temperatures       []string
@@ -58,6 +59,7 @@ func (wd *weatherData) initialize() {
 
 func (wd *weatherData) setForecaseData(data forecast.DataPoint) {
 	weatherTime := time.Unix(int64(data.Time), 0)
+	wd.date = weatherTime
 	wd.times = append(wd.times, fmt.Sprintf("%02d", weatherTime.Hour()))
 	wd.weathers = append(wd.weathers, weatherConversionTable[data.Icon])
 	wd.temperatures = append(wd.temperatures, fmt.Sprintf("%.1f", float32(data.Temperature)))
@@ -66,7 +68,14 @@ func (wd *weatherData) setForecaseData(data forecast.DataPoint) {
 	wd.windSpeeds = append(wd.windSpeeds, fmt.Sprintf("%.1f", convertMilePerHourToMS(float64(data.WindSpeed))))
 }
 
-func (wd *weatherData) renderWeather(w io.Writer) {
+func (wd *weatherData) showDate(w io.Writer) {
+	fmt.Fprintf(w, "┌──────────────┐\n")
+	fmt.Fprintf(w, "│ %d-%02d-%d   │\n", wd.date.Year(), wd.date.Month(), wd.date.Day())
+	fmt.Fprintf(w, "└──────────────┘\n")
+}
+
+func (wd *weatherData) showWeather(w io.Writer) {
+	wd.showDate(w)
 	table := tablewriter.NewWriter(w)
 	table.SetHeader(wd.times)
 	table.Append(wd.weathers)
@@ -76,6 +85,16 @@ func (wd *weatherData) renderWeather(w io.Writer) {
 	table.Append(wd.windSpeeds)
 	table.SetBorder(false)
 	table.Render()
+}
+
+func convertMilePerHourToMS(windSpeed float64) float64 {
+	return windSpeed * 0.447
+}
+
+func convertDegToCompass(deg float64) string {
+	compasses := []string{"北", "北北東", "北東", "東北東", "東", "東南東", "南東", "南南東", "南", "南南西", "南西", "西南西", "西", "西北西", "北西", "北北西"}
+	val := int((deg / 22.5) + .5)
+	return compasses[(val % 16)]
 }
 
 func loadConfig() (*config, error) {
@@ -95,22 +114,6 @@ func loadConfig() (*config, error) {
 	return &cfg, err
 }
 
-func showDay(time time.Time, w io.Writer) {
-	fmt.Fprintf(w, "┌──────────────┐\n")
-	fmt.Fprintf(w, "│ %d-%02d-%d   │\n", time.Year(), time.Month(), time.Day())
-	fmt.Fprintf(w, "└──────────────┘\n")
-}
-
-func convertMilePerHourToMS(windSpeed float64) float64 {
-	return windSpeed * 0.447
-}
-
-func convertDegToCompass(deg float64) string {
-	compasses := []string{"北", "北北東", "北東", "東北東", "東", "東南東", "南東", "南南東", "南", "南南西", "南西", "西南西", "西", "西北西", "北西", "北北西"}
-	val := int((deg / 22.5) + .5)
-	return compasses[(val % 16)]
-}
-
 func main() {
 	config, err := loadConfig()
 
@@ -123,7 +126,6 @@ func main() {
 	var wd weatherData
 	wd.initialize()
 
-	needShowDay := true
 	w := os.Stdout
 
 	f, err := forecast.Get(config.ForecaseApiKey, config.Home.Lat, config.Home.Lng, "now", forecast.AUTO)
@@ -133,19 +135,13 @@ func main() {
 	}
 
 	for _, data := range f.Hourly.Data {
-		weatherTime := time.Unix(int64(data.Time), 0)
-		if needShowDay {
-			showDay(weatherTime, w)
-			needShowDay = false
-		}
-
 		wd.setForecaseData(data)
 
+		weatherTime := time.Unix(int64(data.Time), 0)
 		if weatherTime.Hour() == 23 {
-			wd.renderWeather(w)
+			wd.showWeather(w)
 			wd.initialize()
 			fmt.Fprintf(w, "\n\n")
-			needShowDay = true
 			showDays += 1
 		}
 
